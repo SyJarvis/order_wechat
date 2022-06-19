f4ae6bd14f5430b6fc5a035cececcda6ecac3598
SyJarvis <1755115828@qq.com>
2022-06-19 16:53:14 +0800

    '启动入口'

---
+++ manager.py
@@ -0,0 +1,29 @@
+# -*- coding: UTF-8 -*-
+# @Time     : 2022/6/19 13:50
+# @Author   : Runke Zhong
+# @Software : PyCharm
+
+
+from flask_script import Manager, Server
+from flask_migrate import MigrateCommand, Migrate
+from application import app, db
+from common.libs.exts import create_db
+
+manager = Manager(app)
+migrate = Migrate(app, db)
+manager.add_command("db", MigrateCommand)
+manager.add_command("create_db", create_db())
+manager.add_command("runserver", Server(host='127.0.0.1', port=8000, use_debugger=True))
+from common.models import *
+
+def main():
+    manager.run()
+
+
+if __name__ == '__main__':
+    try:
+        import sys
+        sys.exit(main())
+    except Exception as e:
+        import traceback
+
